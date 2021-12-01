4a77376767f11a880682a40265e21f8ac77083f8
Roshaad Johnson <roshaadjohnson@gmail.com>
2021-11-30 18:52:20 -0600

    change

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY");
 var date = document.getElementById("currentDay")
 date.textContent = currentDate
+
+$(".saveBtn").click(function(){
+console.log($(this).siblings(".col-10").val().trim())
+});
